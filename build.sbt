7c3949b448d513b174fb52e8a684cc7258c65422
DuboisRemi <dbs.remi@gmail.com>
2018-12-04 14:46:24 +0100

    now save the logs in cassandra db

--- build.sbt
+++ build.sbt
@@ -9,7 +9,9 @@ scalaVersion := "2.11.12"
 val sparkVersion = "2.2.0"
 
 //refer at https://mvnrepository.com
-libraryDependencies ++= Seq{
-  "org.apache.spark" %% "spark-core" % sparkVersion;
-  "org.apache.spark" %% "spark-sql" % sparkVersion
-}
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "com.datastax.spark" %% "spark-cassandra-connector" % "2.4.0")
+  .map(_.exclude("org.slf4j","log4j-over-slf4j")
+)
--- src/main/scala/analyzer.scala
+++ src/main/scala/analyzer.scala
@@ -1,9 +1,8 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkConf
-import org.apache.spark._
 import org.apache.spark.sql._
+import org.apache.spark.sql.cassandra._
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
+import com.datastax.spark.connector.cql.CassandraConnectorConf
 
 object analyzer {
 
@@ -15,13 +14,24 @@ object analyzer {
       .master("local[*]")
       .appName("Spark Log Analyzer")
       .getOrCreate
+    spark.setCassandraConf("localCluster",CassandraConnectorConf.ConnectionHostParam.option("127.0.0.1"))
 
-    val logs = logsReader("access.log",spark)
+    val logs = logsParser(spark.read.option("delimiter", " - - ").text("src/main/resources/access.log"))
+
+    //save logs in Cassandra Table
+    println(logs.count())
+    logs.write
+      .cassandraFormat("logs","logs_spark","localCluster")
+      .mode("overwrite")
+      .option("confirm.truncate", true)
+      .save()
+
+
+
     logs.show(5)
-    logs.groupBy("request method").count().show()
-    logs.groupBy("type request status").count().show()
-    logs.groupBy("request status").count().show()
-    logs.groupBy("time zone").count().show()
+    logs.groupBy("request_method").count().show()
+    logs.groupBy("type_request_status").count().show()
+    logs.groupBy("request_status").count().show()
     countTimeAttendance(logs).show()
     countServerErrorsPerHour(logs).show()
     val t1 = System.currentTimeMillis()
@@ -32,21 +42,19 @@ object analyzer {
 
   /* Read a log fil in resources
    * load it and retrun it into a dataframe  */
-  def logsReader(path:String,spark: SparkSession ) : DataFrame = {
+  def logsParser(logs : DataFrame ) : DataFrame = {
 
-    var logs = spark.read.option("delimiter", " - - ").text("src/main/resources/" + path)
-    logs = logs
-      .withColumn("Ip", split(col("value"), " - - ").getItem(0))
+    val parsedLogs = logs.withColumn("ip", split(col("value"), " - - ").getItem(0))
       .withColumn("others", split(col("value"), " - - ").getItem(1))
       .withColumn("time", split(col("others"), "\"").getItem(0))
       .withColumn("request", split(col("others"), "]").getItem(1))
       .drop("value")
       .drop("others")
 
-    logs = logsFormatTime(logs)
-    logs = logsFormatRequest(logs)
-    logs = getTypeRequestStatus(logs)
-    logs
+    val formatedTimeLogs = logsFormatTime(parsedLogs)
+    val formatedRequestLogs = logsFormatRequest(formatedTimeLogs)
+    val typedRequestLogs = getTypeRequestStatus(formatedRequestLogs)
+    typedRequestLogs.drop("request")
   }
 
   /*Split time into Date and Hour */
@@ -58,7 +66,6 @@ object analyzer {
     logs = logs
       .withColumn("date", split(col("time"), ":").getItem(0))
       .withColumn("time", getTime(logs("time")))
-      .withColumn("time zone", split(col("time")," ").getItem(1))
     logs
   }
 
@@ -68,19 +75,19 @@ object analyzer {
     var logs = inputLogs.withColumn("request", formatRequest(inputLogs("request")) )
 
     logs = logs
-      .withColumn("request method",split(col("request")," ").getItem(0))
-      .withColumn("request path",split(col("request")," ").getItem(1))
-      .withColumn("request protocol",split(col("request")," ").getItem(2))
-      .withColumn("request protocol",substring(col("request protocol"),0,8))
-      .withColumn("request status",split(col("request")," ").getItem(3))
-      .withColumn("request content",split(col("request")," ").getItem(4))
-      .withColumn("referrer path",split(col("request")," ").getItem(5))
+      .withColumn("request_method",split(col("request")," ").getItem(0))
+      .withColumn("request_path",split(col("request")," ").getItem(1))
+      .withColumn("request_protocol",split(col("request")," ").getItem(2))
+      .withColumn("request_protocol",substring(col("request_protocol"),0,8))
+      .withColumn("request_status",split(col("request")," ").getItem(3))
+      .withColumn("request_content",split(col("request")," ").getItem(4))
+      .withColumn("referrer_path",split(col("request")," ").getItem(5))
 
-    logs = logs.filter(logs("request status") =!= "null")
+    logs = logs.filter(logs("request_status") =!= "null")
 
-    logs = logs.filter(logs("request method") =!= "T")
-    logs = logs.withColumn("request method",when(col("request method")=== "Head", "HEAD")
-              .otherwise(col("request method")))
+    logs = logs.filter(logs("request_method") =!= "T")
+    logs = logs.withColumn("request_method",when(col("request_method")=== "Head", "HEAD")
+              .otherwise(col("request_method")))
     logs
   }
 
@@ -92,19 +99,19 @@ object analyzer {
       else if (status.startsWith("4")) "Client error"
       else if (status.startsWith("5")) "Server error"
       else "Unknown Status")
-    inputLogs.withColumn("type request status",typeRequest(col("request status")))
+    inputLogs.withColumn("type_request_status",typeRequest(col("request_status")))
     }
 
   //Count number of request by hour
   def countTimeAttendance(inputLogs : DataFrame) : DataFrame = {
-    var logs = inputLogs.withColumn("hour",split(col("time"), ":").getItem(0))
+    val logs = inputLogs.withColumn("hour",split(col("time"), ":").getItem(0))
     logs.groupBy("hour").count().orderBy(desc("count"))
   }
 
   //Count the number of server errors per hour
   def countServerErrorsPerHour(inputLogs :DataFrame) : DataFrame = {
-    var errorsPerHour = inputLogs
-      .filter(inputLogs("type request status") === "Server error")
+    val errorsPerHour = inputLogs
+      .filter(inputLogs("type_request_status") === "Server error")
       .withColumn("hour",split(col("time"), ":").getItem(0))
       .groupBy("hour").count().orderBy(desc("count"))
     errorsPerHour
